882dd40d926c7086cfee4b5efa6ec079d02a00a8
warda <warda@care3.co>
2019-07-30 18:59:00 +0000

    set up a mutation resolver for items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutation = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    return item;
+  }
+};
 
-module.exports = Mutation;
+module.exports = Mutations;
