3734ab9c9f8d1c699bed8900cf8d0ee3f2e65f6d
warda <warda@care3.co>
2019-08-01 19:46:37 +0000

    created Pagination component

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+
+const Pagination = props => {
+  return <p>Pagination bruh</p>
+}
+
+export default Pagination;
