c898d563f94d1bc14a6a082cb382938b0ef56347
warda <warda@care3.co>
2019-08-01 21:00:13 +0000

    added page count + prev and next links

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -2,6 +2,8 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import PaginationStyles from './styles/PaginationStyles';
+import Head from 'next/head';
+import Link from 'next/link';
 
 const PAGINATION_QUERY = gql`
   query PAGINATION_QUERY {
@@ -16,10 +18,27 @@ const PAGINATION_QUERY = gql`
 const Pagination = props => {
   return (
     <PaginationStyles>
-      <Query query={PAGINATION_QUERY}>
-        {({ data, loading, error }) => (
-          <p>Pagination bruh</p>
-        )}
+            <Head>
+              <title>Sick Fits | Page {page} of {pages}</title>
+            </Head>
+            <Link
+              prefetch
+              href={{
+                pathname: 'items',
+                query: { page: page - 1 }
+              }}>
+              <a className="prev" aria-disabled={page <= 1}>← Prev</a>
+            </Link>
+            <p>Page {page} of {pages}</p>
+            <p>{count} Items Total</p>
+            <Link
+              prefetch
+              href={{
+                pathname: 'items',
+                query: { page: page + 1 }
+              }}>
+              <a className="next" aria-disabled={page >= pages}>Next →</a>
+            </Link>
       </Query>
     </PaginationStyles>
   )
