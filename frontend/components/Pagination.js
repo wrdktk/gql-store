524abed3d9a6d5731cbd321b2d13a2a527322206
warda <warda@care3.co>
2019-08-01 20:11:36 +0000

    created Pagination query

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import PaginationStyles from './styles/PaginationStyles';
 
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    itemsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
+
 const Pagination = props => {
-  return <p>Pagination bruh</p>
+  return (
+    <PaginationStyles>
+      <Query query={PAGINATION_QUERY}>
+        {({ data, loading, error }) => (
+          <p>Pagination bruh</p>
+        )}
+      </Query>
+    </PaginationStyles>
+  )
 }
 
 export default Pagination;
