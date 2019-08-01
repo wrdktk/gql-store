533df02f1e8b45d8bf32e9ed7e8446245fb21a18
warda <warda@care3.co>
2019-08-01 21:27:26 +0000

    updated all items query for displaying perPage items

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,10 +4,11 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(skip:$skip, first:$first, orderBy: id_DESC) {
       id
       title
       price
@@ -34,7 +35,12 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          fetchPolicy="network-only"
+          query={ALL_ITEMS_QUERY} variables={{
+            skip: this.props.page * perPage - perPage,
+            first: perPage
+          }}>
           {({ data, error, loading }) => {
             if (loading) {
               return <p>loading...</p>;
