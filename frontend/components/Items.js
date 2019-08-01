08bb48d3d2b070d18634dd69e89267134ffd15b6
warda <warda@care3.co>
2019-08-01 21:27:55 +0000

    commented out fetchPolicy

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -36,7 +36,7 @@ class Items extends Component {
       <Center>
         <Pagination page={this.props.page} />
         <Query
-          fetchPolicy="network-only"
+          // fetchPolicy="network-only"
           query={ALL_ITEMS_QUERY} variables={{
             skip: this.props.page * perPage - perPage,
             first: perPage
