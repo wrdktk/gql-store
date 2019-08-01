ba459dbc4ad0bdf309851cca809c3a9f33e8965c
warda <warda@care3.co>
2019-08-01 16:39:49 +0000

    added an updateItem mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -4,9 +4,23 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem({
       data: {
         ...args
-      }
+      },
     }, info);
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    //first take a copy of the updates
+    const updates = { ...args };
+    //remove id from the updates
+    delete updates.id;
+    //run the updates method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info)
   }
 };
 
