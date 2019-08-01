13cd43ed9cadb83bd3c0206cecc540bef6f95bfd
warda <warda@care3.co>
2019-08-01 17:47:07 +0000

    added a resolver for delete item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -10,17 +10,28 @@ const Mutations = {
   },
 
   updateItem(parent, args, ctx, info) {
-    //first take a copy of the updates
+    // first take a copy of the updates
     const updates = { ...args };
-    //remove id from the updates
+    // remove id from the updates
     delete updates.id;
-    //run the updates method
+    // run the updates method
     return ctx.db.mutation.updateItem({
       data: updates,
       where: {
         id: args.id
-      }
-    }, info)
+      },
+    }, info);
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // 2. check if user owns item / has permission to delete item
+    //TODO
+    // 3. delete item
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
