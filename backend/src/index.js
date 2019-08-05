ef3ee90e085c244c40b3848d13556acd1a171bd1
warda <warda@care3.co>
2019-08-05 18:16:48 +0000

    decoded jwt token for userId

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,23 @@
 require("dotenv").config({ path: "variables.env" });
 const cookieParser = require("cookie-parser");
 const createServer = require("./createServer");
+const jwt = require('jsonwebtoken '); 
 const db = require("./db");
 
 const server = createServer();
 
-// TODO Use express middleware to handle cookies
 server.express.use(cookieParser());
 
-// TODO Use express middleware to populate current user
+// decode JWT so we can grab user id for each request
+server.express.use(req, res, next) {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+     // put the userId on req for further requests to access
+    req.userId = userId;
+  }
+  next();
+}
 
 server.start(
   {
