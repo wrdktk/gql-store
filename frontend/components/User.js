00c9165c895704fac31fd182b83297c1a8ca74b1
warda <warda@care3.co>
2019-08-05 19:00:23 +0000

    added userId to props and Query as custom render prop

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -3,10 +3,25 @@ import gql from "graphql-tag";
 import PropTypes from "prop-types";
 
 const CURRENT_USER_QUERY = gql`
-  me{
-    id
-    name
-    email
-    permissions
+  query CURRENT_USER_QUERY {
+    me {
+      id
+      name
+      email
+      permissions
+    }
   }
 `;
+
+const User = props => (
+  <Query {...props} query={CURRENT_USER_QUERY}>
+    {payload => props.children(payload)}
+  </Query>
+);
+
+User.PropTypes = {
+  children: PropTypes.func.isRequired
+};
+
+export default User;
+export { CURRENT_USER_QUERY };
