4970d88bc365bfbae64ae9f6b977697daec11f9f
warda <warda@care3.co>
2019-08-07 18:53:45 +0000

    created a signout component

---
+++ frontend/components/Signout.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import { CURRENT_USER_QUERY } from "./User";
+
+const Signout = props => {
+  <button>Sign Out</button>;
+};
+
+export default Signout;
