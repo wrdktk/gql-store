eeb795528a41418b11be9b284f1dd68e8902c7cd
warda <warda@care3.co>
2019-08-07 18:45:23 +0000

    reorganized nav to show links based on user log in status

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,32 +3,36 @@ import NavStyles from "./styles/NavStyles";
 import User from "./User";
 
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {({ data: { me } }) => {
-        if (!me) {
-          return null;
-        }
-        return <p>{me.name}</p>;
-      }}
-    </User>
-
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data: { me } }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+            <Link href="/signup">
+              <a>Sign Out</a>
+            </Link>
+          </>
+        )}
+        {!me && (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
