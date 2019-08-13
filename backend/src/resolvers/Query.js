25c19718593c6bb16d78e5462a43c4b6be338ea0
warda <warda@care3.co>
2019-08-13 23:00:17 +0000

    query all users - check if user is logged in

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,10 @@
-const { forwardTo } = require("prisma-binding");
+const { forwardTo } = require('prisma-binding');
+const { hasPermissions } = require('../utils');
 
 const Query = {
-  items: forwardTo("db"),
-  item: forwardTo("db"),
-  itemsConnection: forwardTo("db"),
+  items: forwardTo('db'),
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   me(parent, args, ctx, info) {
     // checl if there's a current user Id
     if (!ctx.request.userId) {
@@ -11,11 +12,18 @@ const Query = {
     }
     return ctx.db.query.user(
       {
-        where: { id: ctx.request.userId }
+        where: { id: ctx.request.userId },
       },
-      info
+      info,
     );
-  }
+  },
+
+  async users(parent, args, ctx, info) {
+    // 1. check if logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in');
+    }
+  },
 };
 
 module.exports = Query;
