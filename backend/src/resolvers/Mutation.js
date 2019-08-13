527a5e328fcb709b1cb01dc97362b7cc289687a2
warda <warda@care3.co>
2019-08-13 18:57:34 +0000

    added User type relationship to Item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,10 +7,18 @@ const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!');
+    }
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          //This is how we create a relationship between item and user
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          }
           ...args,
         },
       },
