f7ce3fd0f62dc10b1cca36bb4170e5acbfd94cdc
warda <warda@care3.co>
2019-08-14 20:11:53 +0000

    created Permissions table and user component

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,6 +1,7 @@
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Error from './ErrorMessage';
+import Table from './styles/Table';
 
 const ALL_USERS_QUERY = gql`
   query ALL_USERS_QUERY{
@@ -12,17 +13,51 @@ const ALL_USERS_QUERY = gql`
     } 
   }
 `
-
+const possiblePermissions = [
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE'
+]
  
 const Permissions = props => (
   <Query query={ALL_USERS_QUERY}>
     {({ data, loading, error }) => (
     <div>
       <Error error={error} />
-      <p>hey</p>
+        <h2>Manage Permissions </h2>
+        <Table>
+          <thead>
+            <tr>
+              <td>NAME</td>
+              <td>EMAIL</td>
+              {possiblePermissions.map(permission =>
+                <th>{permission}</th>)}
+              <th>UPDATE</th>
+            </tr>
+          </thead>
+          <tbody>
+           {data.users.map(user=><User user={user}/>)}
+          </tbody>
+        </Table>
     </div>
     )}
   </Query>
 )
+
+class User extends React.Component {
+  render() {
+    const user = this.props.user;
+    return (
+
+      <tr>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+      </tr>
+    )
+  }
+}
 
 export default Permissions;
