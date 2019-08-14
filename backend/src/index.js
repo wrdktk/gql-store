3c4e342b63f8bc06c034a71089d5547a1efe2323
warda <warda@care3.co>
2019-08-14 19:27:54 +0000

    added middleware to add user to req

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
-require("dotenv").config({ path: "variables.env" });
-const cookieParser = require("cookie-parser");
-const createServer = require("./createServer");
-const jwt = require("jsonwebtoken");
-const db = require("./db");
+require('dotenv').config({ path: 'variables.env' });
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const createServer = require('./createServer');
+const db = require('./db');
 
 const server = createServer();
 
@@ -19,14 +19,22 @@ server.express.use((req, res, next) => {
   next();
 });
 
+// popular user on each request
+server.express.use(async (req, res, next) => {
+  // if user is not logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user({ where: { id: req.userId } }, '{id, name, email, permissions}');
+});
+
+
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
+      origin: process.env.FRONTEND_URL,
+    },
   },
-  deets => {
+  (deets) => {
     console.log(`Server is now running on http://localhost:${deets.port}`);
-  }
+  },
 );
