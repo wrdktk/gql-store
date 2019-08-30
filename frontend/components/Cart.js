6deaccac49889764ca646f7737ab69a414b9b989
warda <warda@care3.co>
2019-08-30 21:40:03 +0000

    added footer section to cart

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -5,11 +5,17 @@ import Supreme from './styles/Supreme';
 import SickButton from './styles/SickButton';
 
 const Cart = () => {
-  return <CartStyles>
+  return <CartStyles open>
     <header>
       <CloseButton title="close">&times;</CloseButton>
       <Supreme>Your Cart</Supreme>
       <p>You have __ items in your cart!</p>
     </header>
+    <footer>
+      <p>$10.00</p>
+      <SickButton>Checkout</SickButton>
+    </footer>
   </CartStyles>
 }
+
+export default Cart;
