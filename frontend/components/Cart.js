b7f1b273aaa882eb59f3ebb1e1c52f4aeee995f4
warda <warda@care3.co>
2019-08-30 22:20:20 +0000

    added open/close functionality

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -2,39 +2,46 @@ import React from 'react';
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import CartStyles from './styles/CartStyles';
-import CloseButton from './styles/CloseButton';
 import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
 
 const LOCAL_STATE_QUERY = gql`
   query {
-    cartOpen @client 
+    cartOpen @client
   }
 `;
+
 const TOGGLE_CART_MUTATION = gql`
   mutation {
-    toggleCart @client 
+    toggleCart @client
   }
 `;
-  
+
 const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
-    <Query query={LOCAL_STATE_QUERY}>{
-      ({ data }) => (
-        <CartStyles open={data.openCart}>
-      <header>
-        <CloseButton onClick={toggleCart } title="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You have __ items in your cart!</p>
-      </header>
-      <footer>
-        <p>$10.00</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyles>
-  )}</Query>
-  )}</Mutation>
+  <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {toggleCart => (
+      <Query query={LOCAL_STATE_QUERY}>
+        {({ data }) => (
+          <CartStyles open={data.cartOpen}>
+            <header>
+              <CloseButton onClick={toggleCart} title="close">
+                &times;
+              </CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You Have __ Items in your cart.</p>
+            </header>
+
+            <footer>
+              <p>$10.10</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyles>
+        )}
+      </Query>
+    )}
+  </Mutation>
 );
 
 export default Cart;
-export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION }; 
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
