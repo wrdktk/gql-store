268afbb1731c3a71aafb5d3bdfecded4f8e4c58e
warda <warda@care3.co>
2019-08-30 21:50:10 +0000

    added query for local state of cart open

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import CartStyles from './styles/CartStyles';
 import CloseButton from './styles/CloseButton';
 import Supreme from './styles/Supreme';
 import SickButton from './styles/SickButton';
 
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client 
+  }
+`;
+  
 const Cart = () => {
-  return <CartStyles open>
-    <header>
-      <CloseButton title="close">&times;</CloseButton>
-      <Supreme>Your Cart</Supreme>
-      <p>You have __ items in your cart!</p>
-    </header>
-    <footer>
-      <p>$10.00</p>
-      <SickButton>Checkout</SickButton>
-    </footer>
-  </CartStyles>
+  <Query query={LOCAL_STATE_QUERY}>{
+    ({ data }) => (
+    
+    <CartStyles open>
+      <header>
+        <CloseButton title="close">&times;</CloseButton>
+        <Supreme>Your Cart</Supreme>
+        <p>You have __ items in your cart!</p>
+      </header>
+      <footer>
+        <p>$10.00</p>
+        <SickButton>Checkout</SickButton>
+      </footer>
+    </CartStyles>
+  )}</Query>
 }
 
 export default Cart;
